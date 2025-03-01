7996099e5486c3aaad550986e8e92572b22a9d91
r-iradukunda <r.iradukund@alustudent.com>
2025-03-01 22:27:03 +0200

    Apis

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python3
+"""
+    script that prints the location of a specific user:
+"""
 
-""" Fetch and print the location of a GitHub user """
 
 import requests
-import sys
 import time
+from datetime import datetime
+
+
+def main(url):
+    """
+    - The user is passed as first argument of the script
+    with the full API URL, example: ./2-user_location.py
+    https://api.github.com/users/holbertonschool
+    - If the user doesn’t exist, print Not found
+    - If the status code is 403, print Reset in X min where X
+    is the number of minutes from now and the value of
+    X-Ratelimit-Reset
+    - Your code should not be executed when the file is
+    imported (you should use if __name__ == '__main__':)
+
+    """
+    response = requests.get(url)
+
+    if response.status_code == 404:
+        print("Not found")
+    elif response.status_code == 403:
+        reset_timestamp = int(response.headers["X-Ratelimit-Reset"])
+        current_timestamp = int(time.time())
+        reset_in_minutes = (reset_timestamp - current_timestamp) // 60
+        print("Reset in {} min".format(reset_in_minutes))
+    else:
+        print(response.json()["location"])
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./2-user_location.py https://api.github.com/rate_limit")
-        sys.exit(1)
+    import sys
 
-    url = sys.argv[1]
-    res = requests.get(url)
-
-    if res.status_code == 403:  # Rate limit exceeded
-        rate_limit = int(res.headers.get("X-Ratelimit-Reset", 0))
-        current_time = int(time.time())
-        diff = (rate_limit - current_time) // 60
-        print("Reset in {} min".format(diff))
-    
-    elif res.status_code == 404:  # User not found
-        print("Not found")
-
-    elif res.status_code == 200:  # Success
-        data = res.json()
-        print(data.get("location", "No location available"))
-    
-    else:  # Handle unexpected errors
-        print("Error:", res.status_code)
-    
+    main(sys.argv[1])
