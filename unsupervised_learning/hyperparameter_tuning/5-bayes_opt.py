e44bd5216d1e97e937b0663d6c013938b5507b36
r-iradukunda <r.iradukund@alustudent.com>
2025-06-25 22:04:21 +0200

    Hyperparameter

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -16,22 +16,6 @@ class BayesianOptimization:
                  sigma_f=1, xsi=0.01, minimize=True):
         """
         Class constructor
-        Args:
-            f: black-box function to be optimized
-            X_init: np.ndarray - (t, 1) - inputs already sampled with the
-                black-box function
-            Y_init: np.ndarray - (t, 1) - outputs of the black-box function
-                for each input in X_init
-            bounds: tuple (min, max) - bounds of the space in which to look
-                for the optimal point
-            ac_samples: number of samples that should be analyzed during
-                acquisition
-            l: length parameter for the kernel
-            sigma_f: standard deviation given to the output of the
-                black-box function
-            xsi: exploration-exploitation factor for acquisition
-            minimize: bool determining whether optimization should be
-                performed for minimization (True) or maximization (False)
         """
         MIN, MAX = bounds
 
@@ -43,45 +27,47 @@ class BayesianOptimization:
 
     def acquisition(self):
         """
-        Calculates the next best sample location
-        Uses the Expected Improvement acquisition function
-        Returns: X_next, EI
+        Calculates the next best sample location using Expected Improvement
         """
         mu, _ = self.gp.predict(self.gp.X)
         sample_mu, sigma = self.gp.predict(self.X_s)
 
         if self.minimize:
             opt_mu = np.min(mu)
+            imp = opt_mu - sample_mu - self.xsi
         else:
             opt_mu = np.max(mu)
+            imp = sample_mu - opt_mu - self.xsi
 
-        imp = opt_mu - sample_mu - self.xsi
-        Z = imp / sigma
-        EI = ((imp * norm.cdf(Z)) + (sigma * norm.pdf(Z)))
-        EI[sigma == 0.0] = 0.0
+        with np.errstate(divide='warn'):
+            Z = imp / sigma
+            EI = (imp * norm.cdf(Z)) + (sigma * norm.pdf(Z))
+            EI[sigma == 0.0] = 0.0
 
         X_next = self.X_s[np.argmax(EI)]
 
-        return X_next, np.array(EI)
+        return X_next, EI
 
     def optimize(self, iterations=100):
         """
         Optimizes the black-box function
-        Args:
-            iterations: maximum number of iterations to perform
-
-        Returns: X_opt, Y_opt
         """
-        for i in range(iterations):
+        for _ in range(iterations):
             X_next, _ = self.acquisition()
 
-            if X_next in self.gp.X:
+            # FIX: robust row-wise float comparison to avoid duplicates
+            if np.any(np.all(np.isclose(self.gp.X, X_next), axis=1)):
                 break
 
-            Y = self.f(X_next)
-            self.gp.update(X_next, Y)
+            Y_next = self.f(X_next)
+            self.gp.update(X_next, Y_next)
 
-        idx = np.argmin(self.gp.Y)
+        if self.minimize:
+            idx = np.argmin(self.gp.Y)
+        else:
+            idx = np.argmax(self.gp.Y)
+
         X_opt = self.gp.X[idx]
-        Y_opt = np.array(self.gp.Y[idx])
+        Y_opt = self.gp.Y[idx]
+
         return X_opt, Y_opt
